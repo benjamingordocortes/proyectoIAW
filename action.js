cdb038ca5303db3032c66755b1b4c31da4a90399
Benjamín Gordo <gordocortesbenjamin@gmail.com>
2020-12-27 16:37:35 +0100

    alfa

---
+++ action.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+    $("article").on("click", function (ev) {
+        $(ev.currentTarget).toggleClass("seleccion");
+        var rut = ev.currentTarget.id;
+        if ($("." + rut).is(":checked")) {
+            $("." + rut).prop("checked", false);
+        } else {
+            $("." + rut).prop("checked", true);
+        }
+
+    });
+
+    $("input[type=checkbox]").on("click", function (et) {
+        var rut1 = $(et.currentTarget).val();
+        $("#" + rut1).toggleClass("seleccion");
+    });
+
+});
+
+
+/*
+$("." + rut).prop("checked", true);*/
